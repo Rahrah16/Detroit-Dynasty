9c873fc2804cb4273c822b4966a6eeb3b7670ffb
Rahrah16 <103587029+Rahrah16@users.noreply.github.com>
2022-04-13 12:27:15 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -11,8 +11,7 @@ export const homepageText = `
   <p></p>
   <p>It has close to live updates and we can easily post weekly power rankings and other happenings here as well.</p>
   <p></p>
-//   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-//   <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
 `;
 
 /*   STEP 3   */

f4796b4abe35c1f070589e28e0988574ee1ab8d7
Rahrah16 <103587029+Rahrah16@users.noreply.github.com>
2022-04-13 11:36:43 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -29,7 +29,7 @@ export const managers = [
     {
       "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Matt Ayres",
-     // "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+      "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Battle Creek", // (optional)
       "bio": "All I do is Win",
       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
@@ -38,7 +38,7 @@ export const managers = [
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Gary Oak", // Can be anything (usually your rival's name)
-        link: 6, // manager array number within this array, or null to link back to all managers page
+        link: null, // manager array number within this array, or null to link back to all managers page
         image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 0069, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
